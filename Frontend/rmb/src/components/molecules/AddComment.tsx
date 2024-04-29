b12d5fa1611a30389a501742b6cd064ce5f2fb0b
Tehran Doroudchi <tehrandoroudchi@gmail.com>
2024-04-29 14:45:12 +0200

    Add comment field

---
+++ Frontend/rmb/src/components/molecules/AddComment.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+interface AddCommentProps {
+  onSubmit: (comment: string) => void;
+}
+
+const AddComment = ({ onSubmit }: AddCommentProps) => {
+  const [comment, setComment] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    onSubmit(comment);
+    setComment("");
+  };
+
+  return (
+    <div className="w-1/3 flex flex-row bg-main rounded-2xl border-none shadow-2xl shadow-gray-800 p-10 m-1">
+      <textarea
+        className=" w-full h-full bg-main text-white rounded-2xl"
+        placeholder="Add a comment..."
+        value={comment}
+        onChange={handleChange}
+      />
+      <button
+        className="bg-secondary text-black rounded-2xl hover:bg-third"
+        onClick={handleSubmit}
+      >
+        Add Comment
+      </button>
+    </div>
+  );
+};
+
+export default AddComment;
