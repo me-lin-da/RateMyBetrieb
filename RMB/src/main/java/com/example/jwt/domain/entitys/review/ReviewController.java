1b8f8472d82aaad27b9ebb6f62f94c3df8ad46ae
Céline Sinzig <celine.sinzig@gmx.ch>
2024-04-30 15:18:21 +0200

    add post and delete for review

--- RMB/src/main/java/com/example/jwt/domain/entitys/review/ReviewController.java
+++ RMB/src/main/java/com/example/jwt/domain/entitys/review/ReviewController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -36,5 +34,23 @@ public class ReviewController {
         return new ResponseEntity<>(reviews, HttpStatus.OK);
     }
 
+    //ACHTUNG, PK funktioniert noch nicht... Kann vorerst nur mit neuen Companys verwendet werden
+    @PostMapping("/add")
+    public ResponseEntity<Review> addReview(@Validated @RequestBody Review review){
+        Review review1 = reviewService.save(review);
+        return new ResponseEntity<>(review1, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Review> updateReview(@PathVariable UUID id, @Validated @RequestBody Review review){
+        Review review1 = reviewService.updateById(id, review);
+        return new ResponseEntity<>(review1, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable UUID id) {
+        reviewService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
